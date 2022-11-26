a7dcc49a810c053aadc55b734f416bef5c3d59de
Silvar <silvar@ya.ru>
2022-11-26 12:59:28 +0300

    Added go to InfActivity and simple open Activity

--- app/src/main/java/ru/synergy/androidstartproject/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartproject/MainActivity.java
@@ -1,48 +1,70 @@
 package ru.synergy.androidstartproject;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    EditText et;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
 
-    //    TextView textView = (TextView) findViewById(R.id.normal);
+        et = (EditText)findViewById(R.id.et);
+        tv = (TextView)findViewById(R.id.tv);
 
-    //    textView.setText("Text from Java code");
-    //    textView.setTextColor(Color.RED);
+        Button btnstart = (Button)findViewById(R.id.buttonStart);
+        Button btncatch = (Button)findViewById(R.id.buttonCatch);
+        Button btnback = (Button)findViewById(R.id.buttonBack);
 
-//        TextView textView = (TextView) findViewById(R.id.header); //найти среди сейчас существующего на экране
-//        textView.setText("Hello from Java :)");
+        btnstart.setOnClickListener(this);
+        btncatch.setOnClickListener(this);
+        btnback.setOnClickListener(this);
+    }
 
-//        ConstraintLayout constraintLayout = new ConstraintLayout(this);
-//
-//
-//        TextView textView = new TextView(this);
-//        textView.setText(("Hello Android!"));
-//        textView.setTextSize(26);
-//
-//        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
-//                ConstraintLayout.LayoutParams.WRAP_CONTENT,
-//                ConstraintLayout.LayoutParams.WRAP_CONTENT);
-//
-//        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
-//        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-//        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
-//        layoutParams.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID;
-//
-//        textView.setLayoutParams(layoutParams);
-//        constraintLayout.addView(textView);
-//
-//        setContentView(constraintLayout);
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data){
+//        super.onActivityResult(requestCode, resultCode, data);
+//        switch (requestCode){
+//            case RESULT_OK:
+//                tv.setText(data.getStringExtra("et"));
+//        }
+//    }
+
+
+    public void onClick(View v){
+        Intent i;
+        int REQ_C = 1;
+        switch(v.getId()) {
+            case R.id.buttonStart:
+                i = new Intent(this, MainActivity2.class);
+                startActivity(i);
+                break;
+            case R.id.buttonCatch:
+                i = new Intent(this, ToInfActivity.class);
+                String eText = et.getText().toString();
+                i.putExtra("et", eText);
+                startActivity(i);
+                break;
+//            case R.id.buttonBack:
+//                i = new Intent(this, ComeBackActivity.class);
+//                startActivityForResult(i, REQ_C);
+        }
     }
 }
